719107cab1aa2764c189a50136bce429c77c16bd
Ratnesh Chouhan <Ratnesh.gfg@gmail.com>
2022-11-17 21:28:40 +0000

    Initial Commit

---
+++ config-server/src/main/java/com/rc/tech/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.rc.tech.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
